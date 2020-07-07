c0174e27136efd64457b6dcd116c358ace75bc94
root <root@ip-172-31-42-145.ap-south-1.compute.internal>
2020-07-07 10:58:41 +0000

    added the template file and changed the metadata

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,12 +2,14 @@ package 'httpd' do
   action :install
 end
 
-file '/var/www/html/index.html' do
-  content "This is a centos server 
-  Hostname : #{node['hostname']}
-  IPAddress : #{node['ipaddress']}
-  Memory : #{node['memory']['total']}
-  CPU : #{node['cpu']['0']['total']}"
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  action :create
+  notifies :restart , 'service[httpd]' , :immediately
+end
+
+remote_file '/var/www/html/dog_photo.jpg' do
+  source 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goodhousekeeping.com%2Flife%2Fpets%2Fg4531%2Fcutest-dog-breeds%2F&psig=AOvVaw3OgxgsDiYnqO6tL60Hm5ij&ust=1594203608943000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCND6q7H1uuoCFQAAAAAdAAAAABAJ'
 end
 
 service 'httpd' do
